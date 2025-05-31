f7058a8fd3609c6a8f8c5e5e81da0f5ae6f8f60f
Seroh <141598689+Serohman@users.noreply.github.com>
2025-05-31 08:56:25 -0700

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,63 +8,61 @@ import {Header} from "@/components/Header";
 import {ImagePreview} from "@/components/ImagePreview";
 import {ImageProcessing} from "@/components/ImageProcessing";
 import {ImageUpload} from "@/components/ImageUpload";
-import {useImageSegmentation} from "@/hooks/useImageSegmentation";
+//import {useImageSegmentation} from "@/hooks/useImageSegmentation";
 import {Image} from "@/types/image";
-import {applyAlphaMask} from "@/utils/image";
+
+//import {applyAlphaMask} from "@/utils/image";
 
 export default function Home() {
   const [sourceImage, setSourceImage] = useState<Image | undefined>();
   const [outputImage, setOutputImage] = useState<Omit<Image, "raw"> | undefined>(); // Omitting raw data because it's not needed
-  const pipeline = useImageSegmentation("briaai/RMBG-1.4");
-  const progressInfo = pipeline.info;
+  //const pipeline = useImageSegmentation("briaai/RMBG-1.4");
+  const progressInfo = undefined; //pipeline.info;
   const isScreenUpload = !sourceImage;
   const isScreenProcessing = sourceImage && progressInfo;
   const isScreenDownload = outputImage;
 
   const handleSourceChange = (image: Image) => {
     setSourceImage(image);
-    handleImageProcessing(image);
+    //handleImageProcessing(image);
   };
 
-  const handleImageProcessing = async (sourceImage: Image) => {
-    // Get the output/result from the segmentation pipeline
-    const segmentationOutput = await pipeline.start(sourceImage.url);
+  // const handleImageProcessing = async (
+  //   sourceImage: Image) => {
+  //   // Get the output/result from the segmentation pipeline
+  //   const segmentationOutput = await pipeline.start(sourceImage.url);
+  //   // Extract the original image's pixel data
+  //   const sourcePixelData = sourceImage.raw.data;
+  //   // Extract the mask data from the segmentation output
+  //   const maskPixelData = segmentationOutput.mask.data;
+  //   // Apply the mask to the original image
+  //   const processedPixelData = applyAlphaMask(sourcePixelData, maskPixelData);
+  //   // Create and render the output image data object
+  //   createOutputImage(
+  //     sourceImage.name,
+  //     new ImageData(
+  //       processedPixelData as Uint8ClampedArray,
+  //       sourceImage.raw.width,
+  //       sourceImage.raw.height
+  //     )
+  //   );
+  // };
 
-    // Extract the original image's pixel data
-    const sourcePixelData = sourceImage.raw.data;
-
-    // Extract the mask data from the segmentation output
-    const maskPixelData = segmentationOutput.mask.data;
-
-    // Apply the mask to the original image
-    const processedPixelData = applyAlphaMask(sourcePixelData, maskPixelData);
-
-    // Create and render the output image data object
-    createOutputImage(
-      sourceImage.name,
-      new ImageData(
-        processedPixelData as Uint8ClampedArray,
-        sourceImage.raw.width,
-        sourceImage.raw.height
-      )
-    );
-  };
-
-  const createOutputImage = async (name: string, data: ImageData) => {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    canvas.width = data.width;
-    canvas.height = data.height;
-    if (ctx) {
-      ctx.putImageData(data, 0, 0);
-      setOutputImage({
-        url: canvas.toDataURL("image/png"),
-        name,
-      });
-    } else {
-      throw new Error("Unexpected error. Failed to create output image.");
-    }
-  };
+  // const createOutputImage = async (name: string, data: ImageData) => {
+  //   const canvas = document.createElement("canvas");
+  //   const ctx = canvas.getContext("2d");
+  //   canvas.width = data.width;
+  //   canvas.height = data.height;
+  //   if (ctx) {
+  //     ctx.putImageData(data, 0, 0);
+  //     setOutputImage({
+  //       url: canvas.toDataURL("image/png"),
+  //       name,
+  //     });
+  //   } else {
+  //     throw new Error("Unexpected error. Failed to create output image.");
+  //   }
+  // };
 
   const reset = () => {
     setSourceImage(undefined);

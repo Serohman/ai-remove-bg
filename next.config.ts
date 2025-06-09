02aca729d63215fae495d424a9cb0e41cd946299
Seroh <141598689+Serohman@users.noreply.github.com>
2025-06-09 14:36:36 -0700

    Prepare repo for Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  ...(process.env.NODE_ENV === "production" && {
+    output: "export",
+    basePath: "/ai-remove-bg",
+    assetPrefix: "/ai-remove-bg/",
+  }),
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
